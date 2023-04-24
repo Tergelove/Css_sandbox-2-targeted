a88cc1113f11cacc7619bb1b483ba1f24aac1f4b
Tergel <tergel@Tergels-imac.local>
2023-04-24 09:15:06 +0200

    CSS-sandbox2-targeted

---
+++ knowledge-timeline/main.js
@@ -0,0 +1,9 @@
+// /* boxuud 2 talaasaa ursaj garch irj bgaa harin scroll bariig doosh guilgehd yg ymr box hezee garch irj bn ve gedgiig olj togtoohd mun jishee n 2011 gesn logo bs ursaj garj irh ystoi uuniig tanij hiihiin tuld javaScript ashiglana */
+/* javaScropt showcase iig ashiglna */
+// boxuudiig songoj avaad items gesn huvisagchd hadgalj bn. run gedg funkts n tuhain list itemuud dr tatalt hgd if viewport dotr bhiin bol tuhain itemdaa show gesn class nemj ugch bn.
+// Events 
+// hna hze duudaj ajiluulh ve gevl load hh uyd, resize hh uyd, scroll hh uyd show class ajillana
+window.addEventListener('load', run); 
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
+// additional js code must be written
